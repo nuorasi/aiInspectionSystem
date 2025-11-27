1f40da15604076ba57a01e51c53de1b42da8e821
Theodore Economy <teconomy@gmail.com>
2025-11-27 10:31:47 -0500

    Added user hierarchy

---
+++ database/migrations/2025_11_27_151640_add_fields_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('userType')->nullable()->after('email');
+            $table->boolean('is_active')->default(true)->after('userType');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('userType','is_active');
+        });
+    }
+};
