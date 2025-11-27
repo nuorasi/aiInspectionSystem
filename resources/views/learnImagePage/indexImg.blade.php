3f0389ecdf3eb630d56d84bddb162bdaaba783bf
Theodore Economy <teconomy@gmail.com>
2025-11-27 14:39:18 -0500

    Added new controllers

--- resources/views/learnImagePage/indexImg.blade.php
+++ resources/views/learnImagePage/indexImg.blade.php
@@ -6,9 +6,10 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+
+            <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h3 class="text-lg font-semibold mb-4">
                         Upload photos for the AI engine
                     </h3>
