e8d8ab12cf61782f4ca94b78415889456e58940a
Theodore Economy <teconomy@gmail.com>
2025-11-27 11:45:55 -0500

    Added new controllers

--- resources/views/learnImagePage/indexAi.blade.php
+++ resources/views/learnImagePage/indexAi.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-center text-white">
-        <h1 class="text-4xl font-bold">Analyze a new image</h1>
+        <h1 class="text-4xl font-bold">Learn a new image</h1>
         <p class="mt-4">This is a custom Blade page.</p>
     </div>
 @endsection
---
+++ resources/views/reviewYourImagesPage/indexTi.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center text-white">
+        <h1 class="text-4xl font-bold">Review Your analyzed images</h1>
+        <p class="mt-4">This is a custom Blade page.</p>
+    </div>
+@endsection
