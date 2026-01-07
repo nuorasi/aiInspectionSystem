97eb29933a816eccca693beba9c715d4e7b5c353
Theodore Economy <teconomy@gmail.com>
2026-01-06 21:13:42 -0500

    Adding products and product_sizes tables

--- database/migrations/2026_01_07_014915_create_product_sizes_table.php
+++ database/migrations/2026_01_07_014915_create_product_sizes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_sizes', function (Blueprint $table) {
             $table->id();
+            $table->integer('productId');
             $table->string('productName');
             $table->string('size'); //with units
             $table->timestamps();
