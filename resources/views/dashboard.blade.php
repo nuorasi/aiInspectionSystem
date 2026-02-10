d830ce53104f726900d3e3e2fdfe83102a0ed2a0
Theodore Economy <teconomy@gmail.com>
2026-02-10 08:19:53 -0500

    Testing Git with new PHPstorm build

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard___') }}
+            {{ __('Dashboard_____') }}
         </h2>
     </x-slot>
 
