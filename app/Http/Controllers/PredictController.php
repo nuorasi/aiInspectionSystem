fb50861e12a9ece404e5f5d5b5e1ea01cc582ae6
Theodore Economy <teconomy@gmail.com>
2026-03-12 20:45:59 -0400

    fixing analyze issue

--- app/Http/Controllers/PredictController.php
+++ app/Http/Controllers/PredictController.php
@@ -11,34 +11,45 @@ class PredictController extends Controller
     /**
      * @throws ConnectionException
      */
+//    public function upload(Request $request)
+//    {
+//        $request->validate([
+//         'file' => ['required', 'file', 'mimes:jpg,jpeg,png,webp', 'max:15360'], // 15MB
+//           // 'file' =>   'required|image|mimes:jpg,jpeg,png|max:15360',
+//        ]);
+//
+//        $file = $request->file('file');
+//
+//        $apiBase = rtrim(config('services.predict.base_url'), '/'); // e.g. http://104.248.13.114:8000
+//        $predictUrl = $apiBase . '/predict';
+//
+//        $response = Http::timeout(60)
+//            ->attach(
+//                'file',
+//                file_get_contents($file->getRealPath()),
+//                $file->getClientOriginalName()
+//            )
+//            ->post($predictUrl);
+//
+//        if (! $response->successful()) {
+//            return response()->json([
+//                'message' => 'Predict API request failed',
+//                'status' => $response->status(),
+//                'body' => $response->body(),
+//            ], 502);
+//        }
+//
+//        return response()->json($response->json());
+//    }
+
     public function upload(Request $request)
     {
-        $request->validate([
-         'file' => ['required', 'file', 'mimes:jpg,jpeg,png,webp', 'max:15360'], // 15MB
-           // 'file' =>   'required|image|mimes:jpg,jpeg,png|max:15360',
+        dd([
+            'has_file' => $request->hasFile('file'),
+            'all_files' => $request->allFiles(),
+            'all_input' => $request->all(),
         ]);
-
-        $file = $request->file('file');
-
-        $apiBase = rtrim(config('services.predict.base_url'), '/'); // e.g. http://104.248.13.114:8000
-        $predictUrl = $apiBase . '/predict';
-
-        $response = Http::timeout(60)
-            ->attach(
-                'file',
-                file_get_contents($file->getRealPath()),
-                $file->getClientOriginalName()
-            )
-            ->post($predictUrl);
-
-        if (! $response->successful()) {
-            return response()->json([
-                'message' => 'Predict API request failed',
-                'status' => $response->status(),
-                'body' => $response->body(),
-            ], 502);
-        }
-
-        return response()->json($response->json());
     }
+
+
 }
